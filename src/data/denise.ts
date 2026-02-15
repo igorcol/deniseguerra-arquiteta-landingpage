dbefe846b4601aaea7207fa3145460c1578530c9
igorCol <igor.colombini@gmail.com>
2026-02-15 19:09:25 -0300

    initial setup + header

---
+++ src/data/denise.ts
@@ -0,0 +1,8 @@
+export const content = {
+  hero: {
+    title: "Arquitetura que Concretiza Sonhos", // Vamos melhorar essa copy depois
+    subtitle: "Há 10 anos criando espaços que refletem histórias e essência em Sorocaba.",
+    cta: "Iniciar Projeto via WhatsApp",
+    whatsapp: "5515996186178"
+  }
+}
